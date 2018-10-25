e88a6b66825c3b30497d21c321594d9c50753517
Chris Cardle <cdcardle@gmail.com>
2018-10-25 21:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function produceDrivingRange(start, finish) {
   let travelDistance = Math.abs(parseInt(finish) - parseInt(start));
 
-  return function insideFunction(maxDistance){
-    if(travelDistance <= maxDistance) {
-    	return `within range by ${maxDistance - travelDistance}`
+  return function insideFunction(blockRange){
+    if(travelDistance <= blockRange) {
+    	return `within range by ${blockRange - travelDistance}`
     } else {
-      return `${travelDistance - maxDistance} blocks out of range`
+      return `${travelDistance - blockRange} blocks out of range`
     }
   }
 }
