745b33c98e7c99322cb9e5810cde11f66d3e1a9e
Chris Cardle <cdcardle@gmail.com>
2018-10-25 21:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(start, finish) {
   let inside = Math.abs(parseInt(finish) - parseInt(start));
-  
+
   function(distance) {
     if(inside <= distance) {
     	return `within range by ${distance - inside}`
