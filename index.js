e27daaa09331872f70ce7d83a0c899b9dbf0fb3f
Chris Cardle <cdcardle@gmail.com>
2018-10-25 21:31:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function produceDrivingRange(blockRange) {
   return function insideFunction(start, finish){
     let travelDistance = Math.abs(parseInt(finish) - parseInt(start));
-    
+
     if(travelDistance <= blockRange) {
     	return `within range by ${blockRange - travelDistance}`
     } else {
       return `${travelDistance - blockRange} blocks out of range`
     }
+  }
+}
+
+function produceTipCalculator() {
+  return function() {
+    return 0
   }
 }
